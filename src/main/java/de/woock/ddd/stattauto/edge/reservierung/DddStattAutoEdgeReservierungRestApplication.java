e508abd9853770cfb347635980e01e707d6d3ec5
HermannW <HermannW@Noose-1099.oosede.local>
2017-10-20 16:16:46 +0200

    State synchronisiert

--- src/main/java/de/woock/ddd/stattauto/edge/reservierung/DddStattAutoEdgeReservierungRestApplication.java
+++ src/main/java/de/woock/ddd/stattauto/edge/reservierung/DddStattAutoEdgeReservierungRestApplication.java
@@ -3,12 +3,14 @@ package de.woock.ddd.stattauto.edge.reservierung;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
 import de.woock.ddd.stattauto.edge.reservierung.pre.PreFilter;
 
 @EnableZuulProxy
+@EnableHystrix
 @EnableDiscoveryClient
 @SpringBootApplication
 public class DddStattAutoEdgeReservierungRestApplication {
